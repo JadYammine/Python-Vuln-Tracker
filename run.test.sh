5b0feea9800ebe886a3f7f861481692d6aec8dfa
JadYammine <jad_yammine@hotmail.com>
2025-08-07 21:46:20 -0400

    fix(test): add missing --profile test to build command in run.test.sh

--- run.test.sh
+++ run.test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker compose -f docker-compose.dev.yml build --no-cache
+docker compose --profile test -f docker-compose.dev.yml build --no-cache
 docker compose --profile test -f docker-compose.dev.yml run --rm test
